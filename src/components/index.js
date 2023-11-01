ba8913e98470d4c586a1461bfcddca924461a106
Antonio Kamiri <53481074+antoniokamiri@users.noreply.github.com>
2023-11-01 07:21:29 +0300

    Setting up styles, components and assets

---
+++ src/components/index.js
@@ -0,0 +1,29 @@
+import Billing from "./Billing";
+import Business from "./Business";
+import Buttons from "./Buttons";
+import CardDeal from "./CardDeal";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import FeedBackCard from "./FeedBackCard";
+import Footer from "./Footer";
+import GetStarted from "./GetStarted";
+import Hero from "./Hero";
+import NavBar from "./NavBar";
+import Stats from "./Stats";
+import Testimonials from "./Testimonials";
+
+export{
+    NavBar,
+    Billing,
+    Business,
+    Buttons,
+    CardDeal,
+    Clients,
+    CTA,
+    FeedBackCard,
+    Footer,
+    GetStarted,
+    Hero,
+    Stats,
+    Testimonials
+};
