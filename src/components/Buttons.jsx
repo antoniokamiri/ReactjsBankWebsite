bc4d58c72529b405af6b3134722af7c6abab25c3
Antonio Kamiri <53481074+antoniokamiri@users.noreply.github.com>
2023-11-07 10:11:32 +0300

    Business and Button Components

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 
-function Buttons() {
+function Buttons({styles}) {
   return (
-    <div>Buttons</div>
+    <button type='button' className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18] text-primary outline-none ${styles} `}>
+        Get Started
+    </button>
   )
 }
 
