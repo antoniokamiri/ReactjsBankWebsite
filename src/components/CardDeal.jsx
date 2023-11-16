c1ff0801afa1c0807274ffd41889983e8d772515
Antonio Kamiri <53481074+antoniokamiri@users.noreply.github.com>
2023-11-16 11:18:25 +0300

    Card Deal Component

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import {card} from '../assets';
+import styles, {layout} from '../style';
+import Buttons from './Buttons';
 
 function CardDeal() {
   return (
-    <div>CardDeal</div>
+    <section className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>
+          Find a better card deal <br className='sm:block hidden'/> in a few easy steps.
+        </h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          Arcu tortor, oitud ub nsttud at drf inyrtgrt faucibud.Aligurt wuid sliruet ehet msutid tortor.
+        </p>
+
+        <Buttons styles='mt-10' />
+      </div>
+      <div className={layout.sectionImg}>
+        <img src={card} alt="card" className='w-[100%] h-[100%]' />
+      </div>
+
+    </section>
   )
 }
 
